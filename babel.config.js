8000ee99e4b9eefdd3cd93e6e9e52ca6e8f6a256
hanz520 <569691266@qq.com>
2020-01-12 16:12:18 +0800

    提交

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    // '@vue/cli-plugin-babel/preset'
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        "useBuiltIns": false    // 当使用 Vue CLI 来构建一个库或是 Web Component 时，推荐给 @vue/babel-preset-env 传入 useBuiltIns: false 选项。这能够确保你的库或是组件不包含不必要的 polyfills。通常来说，打包 polyfills 应当是最终使用你的库的应用的责任。
+      }
+    ]
   ]
 }
--- examples/App.vue
+++ examples/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     只有一行文字
+    <componentA />
   </div>
 </template>
 
 <script>
+import componentA from './component-a.js'
 export default {
   name: 'app',
   components: {
+    componentA
   }
 }
 </script>
